21cf7ebdb1b996cc85a7a2ae365cab0e1abc13e0
carlos guzman <guzmca@gmail.com>
2014-05-06 20:05:06 -0400

    Processing now works

--- SetCardGameGUI.java
+++ SetCardGameGUI.java
@@ -1,28 +1,16 @@
-import processing.core.*;
+import processing.core.PApplet;
 import java.util.*;
 
 public class SetCardGameGUI extends PApplet {
 	public static void main(String args[]) {
-	    PApplet.main(new String[] { "--present", "SetCardGameGUI" });
-	  	// Game game = new Game();
+	    PApplet.main(new String[] { "SetCardGameGUI" });
+	  	Game game = new Game();
 	}
- //  		PApplet p = new PApplet();
-	// 	p.init();
-	// 	p.size(700, 480);
-	// 	p.background(0,0,0);
-	// }
-// }
-
-// class Processing extends PApplet{
 	private int width = 700, height = 480;
-	// Scanner input = new Scanner(System.in);
+	//Scanner input = new Scanner(System.in);
 
 	public void setup(){
 		size(width, height);
-		// if (frame != null) {
-		// 	frame.setResizable(true);
-		frame.setSize(width, height);
-		// }
 		drawMainMenu();
 	}
 	public void draw(){
